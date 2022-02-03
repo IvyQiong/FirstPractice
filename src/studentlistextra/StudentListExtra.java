4e8b2bab2c4c2a8a35d0fa0872bcabd04deb8e22
IvyQiong <liaoq@sheridancollege.ca>
2022-02-03 16:54:26 -0500

    Put array back to 3

--- src/studentlistextra/StudentListExtra.java
+++ src/studentlistextra/StudentListExtra.java
@@ -6,7 +6,7 @@ package studentlistextra;
  */
 public class StudentListExtra {
     public static void main(String[] args) {
-     Student2[] studentList = new Student2[4];
+     Student2[] studentList = new Student2[3];
      studentList[0] = new Student2("s1","Amy");
      studentList[1] = new Student2("s2","Bob");
      studentList[2] = new Student2("s3","John");
